2c2e3151b7815b04d5d354864143691fa38b961e
Wilson <jessw11@home.ku.edu>
2020-03-16 10:32:59 -0500

    Begin

---
+++ Test.py
@@ -0,0 +1,65 @@
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# %%a
+# start
+# Get vehicle information from the user
+    vehicle = input("Input car make, model, and year(Example: Ford Taurus 2018)\n")
+
+    # Create list from user input
+    vehicleInfo = vehicle.split()
+
+    # Output information
+    print("Manufacturer: ",vehicleInfo[0])
+    print("Model: ",vehicleInfo[1])
+    
