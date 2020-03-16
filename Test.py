077d6404cfbd40aa9c596a1ee46f04b2893ccf5c
Wilson <jessw11@home.ku.edu>
2020-03-16 10:36:12 -0500

    udpates

--- Test.py
+++ Test.py
@@ -53,13 +53,13 @@
 # %%a
 # %%a
 # start
-# Get vehicle information from the user
-    vehicle = input("Input car make, model, and year(Example: Ford Taurus 2018)\n")
+    currentYear = 2020
+    vehicle = input("Input car make, model, vehicle type, and year(Example: Ford Taurus 2018)\n")
 
     # Create list from user input
     vehicleInfo = vehicle.split()
-
+    vehicleAge = currentYear - vehicleInfo[2]
     # Output information
     print("Manufacturer: ",vehicleInfo[0])
     print("Model: ",vehicleInfo[1])
-    
+    print("Age: ",vehicleAge)
