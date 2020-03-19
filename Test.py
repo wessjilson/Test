25084a01b899eab5ce3a3261e1e271ba6dccb86d
Wilson <jessw11@home.ku.edu>
2020-03-19 16:20:51 -0500

    wipe clean

--- Test.py
+++ Test.py
@@ -53,13 +53,3 @@
 # %%a
 # %%a
 # start
-    currentYear = 2020
-    vehicle = input("Input car make, model, vehicle type, and year(Example: Ford Taurus 2018)\n")
-
-    # Create list from user input
-    vehicleInfo = vehicle.split()
-    vehicleAge = currentYear - vehicleInfo[2]
-    # Output information
-    print("Manufacturer: ",vehicleInfo[0])
-    print("Model: ",vehicleInfo[1])
-    print("Age: ",vehicleAge)
