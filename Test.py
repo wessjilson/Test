32c420a710a17533184124e27a0f375e95f1fe29
Wilson <jessw11@home.ku.edu>
2020-03-20 10:33:06 -0500

    green lines with color in example

--- Test.py
+++ Test.py
@@ -53,18 +53,14 @@
 # %%a
 # %%a
 # start
-    currentYear = 2020
-    # Get vehicle information from the user
-    vehicle = input("Input car make, model, color, year, and vehicle type(Example: Ford Taurus 2018 Car)\n")
+# Get vehicle information from the user
+    vehicle = input("Input car make, model, color, and year(Example: Ford Taurus Blue 2018)\n")
 
     # Create list from user input
     vehicleInfo = vehicle.split()
-    vehicleAge = currentYear - vehicleInfo[3]
 
     # Output information
     print("Manufacturer: ",vehicleInfo[0])
     print("Model: ",vehicleInfo[1])
     print("Color: ",vehicleInfo[2])
     print("Year: ",vehicleInfo[3])
-    print("Vehicle Type: ",vehicleInfo[4])
-    print("Age: ",vehicleAge)
